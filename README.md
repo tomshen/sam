5fa4a165d8068c2e8e6b0af8b04fbf1c4aaaa400
Tom Shen <tom@shen.io>
2014-05-17 01:51:10 -0400

    Add SoundCloud voice controls

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-sam
+Sam
 ===
+Voice-driven SoundCloud player.
 
-Always-on voice-controlled digital assistant
+Requires Google Chrome v25+ (for the [Web Speech API](http://updates.html5rocks.com/2013/01/Voice-Driven-Web-Apps-Introduction-to-the-Web-Speech-API))
--- static/js/main.js
+++ static/js/main.js
@@ -11,39 +11,113 @@ var MusicPlayer = (function() {
     SC.get('/tracks', { q: query, filter: 'streamable' }, callback);
   };
 
-  MusicPlayer.prototype.stream = function (track) {
-    SC.stream('/tracks/' + track.id, function (sound) {
-      sound.play();
-    });
+  MusicPlayer.prototype.stream = function (track, callback) {
+    SC.stream('/tracks/' + track.id, callback);
   };
 
   MusicPlayer.prototype.embed = function (track, embedDomNode) {
     SC.oEmbed(track.uri, { auto_play: true }, embedDomNode);
   };
 
+  var trackTemplate = '<div class="track"><img src="${artwork_url}" class="track-art" alt="Album art"><span class="track-header">${uploader}: <a href="${permalink_url}">${title}</a></span><p class="track-description">${description}</p></div>';
+  MusicPlayer.prototype.renderTrack = function (track) {
+    return _.template(trackTemplate, {
+      artwork_url: track.artwork_url.replace('large', 't500x500'),
+      title: track.title,
+      description: track.description.replace(/\n/g, '<br>'),
+      uploader: track.user.username,
+      permalink_url: track.permalink_url
+    });
+  };
+
   return MusicPlayer;
 })();
 
-function initSpeechRecognition(onresult) {
-  var recognition = new webkitSpeechRecognition();
-  recognition.continuous = true;
-  recognition.interimResults = true;
-  recognition.onstart = function () {
-    recognition.lang = 'en';
-  };
-  recognition.onresult = onresult;
-  recognition.onerror = function(err) {
-    console.error(err);
-  };
-  recognition.onend = function() {};
-  recognition.start();
-  return recognition;
-}
-
-function parseAction(text, action, callback) {
-  var tokens = text.split(' ');
-  var searchTokenIndex = tokens.indexOf(action);
-  if (searchTokenIndex !== -1 && tokens.length > searchTokenIndex) {
-    callback(tokens.slice(searchTokenIndex+1).join(' '));
+var SpeechRecognizer = (function() {
+  function initSpeechRecognition(recognizer) {
+    var recognition = new webkitSpeechRecognition();
+    recognition.continuous = true;
+    recognition.interimResults = true;
+    recognition.onstart = function () {
+      recognition.lang = 'en';
+    };
+    recognition.onerror = function (err) {
+      console.error(err);
+    };
+    recognition.onresult = (function (event) {
+      this.updateTranscript('');
+      for (var i = event.resultIndex; i < event.results.length; i++) {
+        if (event.results[i].isFinal) {
+          var finalTranscript = event.results[i][0].transcript;
+          this.updateTranscript(finalTranscript);
+          _.forEach(this.actions, function (handler, action) {
+            if (this.parseAction(finalTranscript, action, handler)) {
+              this.updateTranscript('');
+              return false;
+            }
+          }, this);
+        } else {
+          this.updateTranscript(this.transcript + event.results[i][0].transcript);
+        }
+      }
+      _.forEach(this.actions, function (handler, action) {
+        if (handler.interim && this.parseAction(this.transcript, action, handler)) {
+          this.updateTranscript('');
+          return false;
+        }
+      }, this);
+    }).bind(recognizer);
+    recognizer._recognition = recognition;
   }
-}
+
+  function SpeechRecognizer(actions, onTranscriptChange) {
+    this.actions = actions;
+    this.onTranscriptChange = onTranscriptChange;
+    this.transcript = '';
+    this.listening = false;
+    initSpeechRecognition(this);
+  }
+
+  SpeechRecognizer.prototype.start = function () {
+    if (!this.listening) {
+      this._recognition.start();
+      this.listening = true;
+    }
+  };
+
+  SpeechRecognizer.prototype.stop = function () {
+    if (this.listening) {
+      this._recognition.stop();
+      this.listening = false;
+    }
+  };
+
+  SpeechRecognizer.prototype.restart = function () {
+    initSpeechRecognition(this);
+    this.listening = false;
+    this.start();
+  };
+
+  SpeechRecognizer.prototype.updateTranscript = function (newTranscript) {
+    this.transcript = newTranscript;
+    this.onTranscriptChange(this.transcript);
+  };
+
+  SpeechRecognizer.prototype.parseAction = function (text, action, handler) {
+    var tokens = text.toLowerCase().split(' ');
+    var searchTokens = _.has(handler, 'similar') ? _.clone(handler.similar) : [];
+    searchTokens.push(action);
+    var foundAction = false;
+    _.forEach(searchTokens, function (searchToken) {
+      var searchTokenIndex = tokens.indexOf(searchToken.toLowerCase());
+      if (searchTokenIndex !== -1 && tokens.length > searchTokenIndex + handler.minLength) {
+        handler.callback.bind(this)(tokens.slice(searchTokenIndex+1).join(' '));
+        foundAction = true;
+        return false;
+      }
+    }, this);
+    return foundAction;
+  };
+
+  return SpeechRecognizer;
+})();
